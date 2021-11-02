d081b27870666ad1b9c09398fd3d6b44876e5bef
Rodriguez Estrada Gael <91295508+GaelRE2004@users.noreply.github.com>
2021-11-02 00:30:17 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="index.php"><img src="lgo.png" height="50px" weight="40px" alt="Gael Rodriguez"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="Alumno.php">Alumno</a></li>
+					<li><a href="materias.php">Materias</a></li>
+					<li><a href="contacto.php">Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
